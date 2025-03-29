f20effb0bea452ed18a0838e76f831b760cd565a
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-29 15:01:38 +0900

    add Lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import {Inter} from 'next/font/google';
+import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});// see https://fonts.google.com/specimen/Inter
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700'],});// see https://fonts.google.com/specimen/Lusitana
