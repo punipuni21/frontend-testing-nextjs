a1eb91d6abea7383ebae3beeb6ab7618ce3dae51
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-31 22:00:04 +0900

    fix bug

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const params = new URLSearchParams();
+    const params = new URLSearchParams(searchParams);
     if (term) {
       params.set("query", term);
     } else {
