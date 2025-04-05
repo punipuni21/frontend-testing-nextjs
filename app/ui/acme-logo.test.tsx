e1d27d0d3f067b8cb9ad53f1a07a026509c8b89b
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-05 23:37:19 +0900

    fix test

--- app/ui/acme-logo.test.tsx
+++ app/ui/acme-logo.test.tsx
@@ -1,38 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import { fetchFilteredCustomers } from "@/app/lib/data";
-import Page from "../dashboard/customers/page";
-import { FormattedCustomersTable } from "@/app/lib/definitions";
+import AcmeLogo from "./acme-logo";
 
-// Mock the fetchFilteredCustomers function
-jest.mock("@/app/lib/data", () => ({
-  fetchFilteredCustomers: jest.fn(),
-}));
+describe("AcmeLogo", () => {
+  it("renders the logo with the globe icon and text", () => {
+    const { container } = render(<AcmeLogo />);
 
-// Mock the CustomersTable component
-jest.mock("@/app/ui/customers/table", () => {
-  return jest.fn(() => <div data-testid="customers-table" />);
-});
+    // Check if the text 'Acme' is rendered
+    const logoText = screen.getByText("Acme");
+    expect(logoText).toBeInTheDocument();
 
-describe("Page Component", () => {
-  it("tmp", async () => {
-    // Mock the fetchFilteredCustomers response
-    const mockCustomers: FormattedCustomersTable[] = [
-      {
-        id: "1",
-        name: "John Doe",
-        email: "hoge",
-        image_url: "hoge",
-        total_invoices: 1,
-        total_pending: "0",
-        total_paid: "1",
-      },
-    ];
-    (fetchFilteredCustomers as jest.Mock).mockResolvedValue(mockCustomers);
-
-    const searchParams = Promise.resolve({ query: "test" });
-    const result = await Page({ searchParams: searchParams });
-    render(result);
-    expect(screen.getByTestId("customers-table")).toBeInTheDocument();
-    expect(fetchFilteredCustomers).toHaveBeenCalledWith("test");
+    expect(container).toMatchSnapshot();
   });
 });
